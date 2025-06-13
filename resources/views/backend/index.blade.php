f4cd078a99d531b4a8342d6f19efaaf51af1b8fe
AngelitoCano <166977416+AngelitoCano@users.noreply.github.com>
2025-06-12 19:53:26 -0600

    Update index.blade.php
    Se renombró pagina web para una busqueda más facil al usuario

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> Panel</title>
+    <title> EduApp - Plataforma Educativa</title>
 
     <link href="{{ asset('images/logo.png') }}" rel="icon">
     <!-- Google Font: Source Sans Pro -->
